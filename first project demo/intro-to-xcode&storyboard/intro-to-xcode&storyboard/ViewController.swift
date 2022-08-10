3f3c45cc59da0f54fbfc94b51fb091ce1f38ee01
Tahya Mumtahi <88910928+tahya765@users.noreply.github.com>
2022-08-10 11:37:28 -0400

    Add files via upload

---
+++ first project demo/intro-to-xcode&storyboard/intro-to-xcode&storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intro-to-xcode&storyboard
+//
+//  Created by Scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
